f45300af9e64035b93602c0862889b39fb36c97d
huthaifaTayeh <huthaifazmi@gmail.com>
2021-12-04 23:14:34 +0200

    layout comp init

---
+++ components/layout.js
@@ -0,0 +1,7 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+
+export default function Layout({ children }) {
+	return <Container>{children}</Container>;
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Layout from "../components/layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return <div> TOZ LEILA LOL!</div>;
+	return <Layout> TOZ LEILA LOL!</Layout>;
 }
 
 export default MyApp;
