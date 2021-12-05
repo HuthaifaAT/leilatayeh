e77fcd1f36b6852600011c273fd69038ee6ac0cf
huthaifaTayeh <huthaifazmi@gmail.com>
2021-12-05 02:19:03 +0200

    basic layout with routing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/layout";
+import React from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return <Layout> TOZ LEILA LOL!</Layout>;
+	return (
+		<div>
+			<Component {...pageProps} />
+		</div>
+	);
 }
 
 export default MyApp;
