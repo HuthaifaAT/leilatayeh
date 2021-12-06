911cd49819482b8d231fb9907e787e69cbbee340
huthaifaTayeh <huthaifazmi@gmail.com>
2021-12-06 16:20:13 +0200

    added dark mode for current session

--- components/layout.js
+++ components/layout.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
@@ -10,12 +11,16 @@ import MenuIcon from "@mui/icons-material/Menu";
 import Toolbar from "@mui/material/Toolbar";
 import Breadcrumbs from "@mui/material/Breadcrumbs";
 import Menu from "@mui/material/Menu";
+import Paper from "@mui/material/Paper";
 import MenuItem from "@mui/material/MenuItem";
 import Head from "next/head";
 import Router from "next/router";
 import Link from "next/link";
+import Switch from "@mui/material/Switch";
+import DarkThemeContext from "./DarkThemeContext";
 
 export default function Layout({ children, title }, props) {
+	const colorMode = React.useContext(DarkThemeContext);
 	const [anchorEl, setAnchorEl] = useState(null);
 	const open = Boolean(anchorEl);
 
@@ -42,58 +47,67 @@ export default function Layout({ children, title }, props) {
 		}
 	};
 	return (
-		<Container maxWidth="lg" sx={{ height: "100vh" }}>
-			<Head>
-				<title>{title}</title>
-			</Head>
+		<Paper variant="outlined" sx={{ width: "100%", height: "100vh" }}>
+			<Container maxWidth="lg" sx={{ height: "100%" }}>
+				<Head>
+					<title>{title}</title>
+				</Head>
 
-			<AppBar position="static" color="transparent">
-				<Toolbar>
-					<IconButton
-						size="large"
-						edge="start"
-						color="inherit"
-						aria-label="menu"
-						sx={{ marginInlineEnd: 0.5 }}
-						onClick={handleClick}>
-						<MenuIcon />
-					</IconButton>
-					<Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
-						<MenuItem onClick={handleClose}>Item1</MenuItem>
-						<MenuItem onClick={handleClose}>Item2</MenuItem>
-						<MenuItem onClick={handleClose}>Item3</MenuItem>
-					</Menu>
-					<HomeIcon
-						onClick={() => {
-							Router.push("/");
-						}}
-						sx={{ marginInlineEnd: 2 }}
-					/>
-					<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-						{title}
-					</Typography>
-					<Breadcrumbs
-						seperator="/"
-						maxItems={2}
-						sx={{ paddingInlineEnd: "0px !important" }}>
-						<Button
-							size="small"
-							variant="text"
+				<AppBar position="static" color="transparent">
+					<Toolbar>
+						<IconButton
+							size="large"
+							edge="start"
 							color="inherit"
-							onClick={handleRegClick}>
-							<Link href="/login">Login</Link>
-						</Button>
-						<Button
-							size="small"
-							variant="text"
-							color="inherit"
-							onClick={handleRegClick}>
-							<Link href="/login">Register</Link>
-						</Button>
-					</Breadcrumbs>
-				</Toolbar>
-			</AppBar>
-			<Box sx={{ width: "100%" }}>{children}</Box>
-		</Container>
+							aria-label="menu"
+							sx={{ marginInlineEnd: 0.5 }}
+							onClick={handleClick}>
+							<MenuIcon />
+						</IconButton>
+						<Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
+							<MenuItem onClick={handleClose}>Item1</MenuItem>
+							<MenuItem onClick={handleClose}>Item2</MenuItem>
+							<MenuItem onClick={handleClose}>Item3</MenuItem>
+						</Menu>
+						<HomeIcon
+							onClick={() => {
+								Router.push("/");
+							}}
+							sx={{ marginInlineEnd: 2 }}
+						/>
+						<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+							{title}
+						</Typography>
+						<Breadcrumbs
+							seperator="/"
+							maxItems={2}
+							sx={{ paddingInlineEnd: "0px !important" }}>
+							<Button
+								size="small"
+								variant="text"
+								color="inherit"
+								onClick={handleRegClick}>
+								<Link href="/login">Login</Link>
+							</Button>
+							<Button
+								size="small"
+								variant="text"
+								color="inherit"
+								onClick={handleRegClick}>
+								<Link href="/login">Register</Link>
+							</Button>
+						</Breadcrumbs>
+						<Switch
+							checked={colorMode.darkState}
+							onChange={colorMode.toggleColorMode}
+						/>
+					</Toolbar>
+				</AppBar>
+				<Box sx={{ width: "100%" }}>{children}</Box>
+				<Paper>
+					<Typography variant="h1">Test</Typography>
+				</Paper>
+			</Container>
+		</Paper>
 	);
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,35 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import Layout from "../components/layout";
+import DarkThemeContext from "../components/DarkThemeContext";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import React from "react";
 
+export const ColorModeContext = React.createContext({
+	toggleColorMode: () => {},
+});
+
 function MyApp({ Component, pageProps }: AppProps) {
+	const [isDark, setIsDark] = React.useState(false);
+
+	const toggleColorMode = () => {
+		setIsDark(!isDark);
+	};
+	const darkTheme = createTheme({
+		palette: {
+			mode: isDark ? "dark" : "light",
+		},
+	});
+	const colorModeSettings = {
+		darkState: isDark,
+		setIsDark,
+		toggleColorMode,
+	};
 	return (
-		<div>
-			<Component {...pageProps} />
-		</div>
+		<DarkThemeContext.Provider value={colorModeSettings}>
+			<ThemeProvider theme={darkTheme}>
+				<Component {...pageProps} />
+			</ThemeProvider>
+		</DarkThemeContext.Provider>
 	);
 }
 
